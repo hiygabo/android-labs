03a95d8c48f1b1e00c7a81e77d9580581ca97efa
hiygabo <gaboandiaalave@gmail.com>
2026-09-01 23:37:59 -0400

    Feat(inset) add new features

--- app1/app/src/main/java/com/gabo/app1/MainActivity.java
+++ app1/app/src/main/java/com/gabo/app1/MainActivity.java
@@ -1,7 +1,9 @@
 package com.gabo.app1;
 
 import android.os.Bundle;
-
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,7 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText lblNombre, lblApellido, lblCarnet;
+    private Button btnMostrar, btnSalir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +23,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        lblNombre = findViewById(R.id.lblNombre);
+        lblApellido = findViewById(R.id.lblApellido);
+        lblCarnet = findViewById(R.id.lblCarnet);
+        btnMostrar = findViewById(R.id.btnMostrar);
+        btnSalir = findViewById(R.id.btnSalir);
+
+        btnMostrar.setOnClickListener(v -> {
+
+            String nombre = lblNombre.getText().toString();
+            String apellidos = lblApellido.getText().toString();
+            String carnet = lblCarnet.getText().toString();
+
+            Intent siguiente = new Intent(MainActivity.this, datos_activity.class);
+
+            siguiente.putExtra("nombre", nombre);
+            siguiente.putExtra("apellidos", apellidos);
+            siguiente.putExtra("carnet", carnet);
+            startActivity(siguiente);
+        });
+
+        btnSalir.setOnClickListener(v -> finish());
+
     }
 }
--- app1/app/src/main/java/com/gabo/app1/datos_activity.java
+++ app1/app/src/main/java/com/gabo/app1/datos_activity.java
@@ -1,6 +1,7 @@
 package com.gabo.app1;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +10,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class datos_activity extends AppCompatActivity {
-
+    private TextView idNomCompleto, idCarnet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +21,23 @@ public class datos_activity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        idNomCompleto = findViewById(R.id.idNomCompleto);
+        idCarnet = findViewById(R.id.idCarnet);
+
+        String nombre = getIntent().getStringExtra("nombre");
+        String apellidos = getIntent().getStringExtra("apellidos");
+        String carnet = getIntent().getStringExtra("carnet");
+
+        if (nombre == null) nombre = "";
+        if (apellidos == null) apellidos = "";
+        if (carnet == null) carnet = "";
+
+        String carnetInvertido = new StringBuilder(carnet).reverse().toString();
+        String nombreModificado = nombre + " " + apellidos;
+
+        idNomCompleto.setText(nombreModificado);
+        idCarnet.setText(carnetInvertido);
+
     }
 }
